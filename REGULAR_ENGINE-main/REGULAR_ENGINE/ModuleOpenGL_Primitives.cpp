f404b4e3c4d2d78229be1ae05a45b8872c9f542e
Pau <pau.olmos2002@gmail.com>
2022-10-07 20:27:43 +0200

    cube movement¿? i botons f1,f2,etc

--- REGULAR_ENGINE-main/REGULAR_ENGINE/ModuleOpenGL_Primitives.cpp
+++ REGULAR_ENGINE-main/REGULAR_ENGINE/ModuleOpenGL_Primitives.cpp
@@ -13,6 +13,7 @@
 #include "imgui_menu.h"
 #include "Application.h"
 #include "Primitive.h"
+#include "ModuleInput.h"
 #include <gl/GL.h>
 #include "ModuleOpenGL_Primitives.h"
 
@@ -40,59 +41,161 @@ bool ModuleOpenGL_Primitives::PreUpdate() {
 
 update_status ModuleOpenGL_Primitives::Update(float dt)
 {
+    
+    float Quad[] = { 0.f + x,1.f + y,0.f + z,//X    Plane 1
+                     0.f + x,0.f + y,0.f + z,//Y 
+                     0.f + x,0.f + y,1.f + z,//Z
 
-    float Quad[] = { 0.f,1.f,0.f,//X    Plane 1
-                     0.f,0.f,0.f,//Y 
-                     0.f,0.f,1.f,//Z
+                     0.0f + x,1.f + y,1.f + z,
+                     0.f + x,1.f + y,0.f + z,
+                     0.f + x,0.f + y,1.f + z,
 
-                     0.0f,1.f,1.f,
-                     0.f,1.f,0.f,
-                     0.f,0.f,1.f,
+                     1.f + x,1.f + y,0.f + z,
+                     0.f + x,1.f + y,0.f + z,
+                     0.f + x,1.f + y,1.f + z,
 
-                     1.f,1.f,0.f, 
-                     0.f,1.f,0.f,
-                     0.f,1.f,1.f,
+                     1.f + x,1.f + y,0.f + z,
+                     0.f + x,1.f + y,1.f + z,
+                     1.f + x,1.f + y,1.f + z,
 
-                     1.f,1.f,0.f,
-                     0.f,1.f,1.f,
-                     1.f,1.f,1.f,
+                     0.f + x,0.f + y,1.f + z,
+                     1.f + x,0.f + y,1.f + z,
+                     0.f + x,1.f + y,1.f + z,
 
-                     0.f,0.f,1.f,
-                     1.f,0.f,1.f,
-                     0.f,1.f,1.f,
+                     1.f + x,0.f + y,1.f + z,
+                     1.f + x,1.f + y,1.f + z,
+                     0.f + x,1.f + y,1.f + z,
 
-                     1.f,0.f,1.f,
-                     1.f,1.f,1.f,
-                     0.f,1.f,1.f,
+                     1.f + x, 0.f + y, 1.f + z,
+                     1.f + x, 0.f + y, 0.f + z,
+                     1.f + x, 1.f + y, 0.f + z,
 
-                     1.f, 0.f, 1.f,
-                     1.f, 0.f, 0.f,
-                     1.f, 1.f, 0.f,
+                     1.f + x, 0.f + y, 1.f + z,
+                     1.f + x, 1.f + y, 0.f + z,
+                     1.f + x, 1.f + y, 1.f + z,
 
-                     1.f, 0.f, 1.f,
-                     1.f, 1.f, 0.f,
-                     1.f, 1.f, 1.f,
+                     0.f + x, 0.f + y, 0.f + z,
+                     0.f + x, 1.f + y, 0.f + z,
+                     1.f + x, 1.f + y, 0.f + z,
 
-                     0.f, 0.f, 0.f,
-                     0.f, 1.f, 0.f,
-                     1.f, 1.f, 0.f,
+                     0.f + x, 0.f + y, 0.f + z,
+                     1.f + x, 1.f + y, 0.f + z,
+                     1.f + x, 0.f + y, 0.f + z,
 
-                     0.f, 0.f, 0.f,
-                     1.f, 1.f, 0.f,
-                     1.f, 0.f, 0.f,
+                     0.f + x, 0.f + y, 0.f + z,
+                     1.f + x, 0.f + y, 0.f + z,
+                     1.f + x, 0.f + y, 1.f + z,
 
-                     0.f, 0.f, 0.f, 
-                     1.f, 0.f, 0.f,
-                     1.f, 0.f, 1.f,
-
-                     1.f, 0.f, 1.f,
-                     0.f, 0.f, 1.f,
-                     0.f, 0.f, 0.f,
+                     1.f + x, 0.f + y, 1.f + z,
+                     0.f + x, 0.f + y, 1.f + z,
+                     0.f + x, 0.f + y, 0.f + z,
     };
 
-    /*Wireframe Mode
-      glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-      glLineWidth(2);*/
+    if (App->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN && App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_IDLE) {
+        x++;
+    }
+    if (App->input->GetKey(SDL_SCANCODE_2) == KEY_DOWN && App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_IDLE) {
+        y++;
+    }
+    if (App->input->GetKey(SDL_SCANCODE_3) == KEY_DOWN && App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_IDLE) {
+        z++;
+    }
+    
+    if (App->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN && App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) {
+        x--;
+    }
+    if (App->input->GetKey(SDL_SCANCODE_2) == KEY_DOWN && App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) {
+        y--;
+    }
+    if (App->input->GetKey(SDL_SCANCODE_3) == KEY_DOWN && App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) {
+        z--;
+    }
+    
+    if (App->input->GetKey(SDL_SCANCODE_F1) == KEY_DOWN && App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_DOWN) {
+        wireFrameView = !wireFrameView;
+    }
+    if (wireFrameView == true) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
+    else {
+    }
+
+    if(App->input->GetKey(SDL_SCANCODE_F2) == KEY_DOWN) {
+        depthTest = !depthTest;
+    }
+    if (depthTest == true) {
+        glEnable(GL_DEPTH_TEST);
+
+    }
+    else {
+        glDisable(GL_DEPTH_TEST);
+    }
+    
+    if(App->input->GetKey(SDL_SCANCODE_F3) == KEY_DOWN) {
+        cullFace = !cullFace;
+    }
+    if (cullFace == true) {
+        glEnable(GL_CULL_FACE);
+
+    }
+    else {
+        glDisable(GL_CULL_FACE);
+    }
+    
+    if(App->input->GetKey(SDL_SCANCODE_F4) == KEY_DOWN) {
+        lighting = !lighting;
+    }
+    if (lighting == true) {
+        glEnable(GL_LIGHTING);
+
+    }
+    else {
+        glDisable(GL_LIGHTING);
+    }
+    
+    if(App->input->GetKey(SDL_SCANCODE_F5) == KEY_DOWN) {
+        colorMaterial = !colorMaterial;
+    }
+    if (colorMaterial == true) {
+        glEnable(GL_COLOR_MATERIAL);
+
+    }
+    else {
+        glDisable(GL_COLOR_MATERIAL);
+    }
+    
+    if(App->input->GetKey(SDL_SCANCODE_F6) == KEY_DOWN) {
+        texture2D = !texture2D;
+    }
+    if (texture2D == true) {
+        glEnable(GL_TEXTURE_2D);
+
+    }
+    else {
+        glDisable(GL_TEXTURE_2D);
+    }
+    
+    if(App->input->GetKey(SDL_SCANCODE_F7) == KEY_DOWN) {
+        renderMode = !renderMode;
+    }
+    if (renderMode == true) {
+        glEnable(GL_RENDER_MODE);
+
+    }
+    else {
+        glDisable(GL_RENDER_MODE);
+    }
+    
+    if(App->input->GetKey(SDL_SCANCODE_F8) == KEY_DOWN) {
+        normalize = !normalize;
+    }
+    if (normalize == true) {
+        glEnable(GL_NORMALIZE);
+
+    }
+    else {
+        glDisable(GL_NORMALIZE);
+    }
 
     glEnableClientState(GL_VERTEX_ARRAY);
     glVertexPointer(3, GL_FLOAT, 0, Quad);
@@ -113,7 +216,5 @@ bool ModuleOpenGL_Primitives::PostUpdate() {
 
 bool ModuleOpenGL_Primitives::CleanUp()
 {
-    LOG("Unloading Intro scene");
-
     return true;
 }
--- REGULAR_ENGINE-main/REGULAR_ENGINE/ModuleOpenGL_Primitives.h
+++ REGULAR_ENGINE-main/REGULAR_ENGINE/ModuleOpenGL_Primitives.h
@@ -13,4 +13,17 @@ public:
 	bool PreUpdate();
 	bool CleanUp();
 
+	float x = 0.f;
+	float y = 0.f;
+	float z = 0.f;
+	
+private:
+	bool wireFrameView = false;
+	bool depthTest = false;
+	bool cullFace = false;
+	bool lighting = true;
+	bool colorMaterial = false;
+	bool texture2D = false;
+	bool renderMode = false;
+	bool normalize = false;
 };
