a865a5efdeac18b26d440e1ba6b6be0b4b7236ca
Pau <pau.olmos2002@gmail.com>
2022-10-10 19:30:18 +0200

    TRYING TO LOAD

--- REGULAR_ENGINE-main/REGULAR_ENGINE/ModuleLoadFBX.cpp
+++ REGULAR_ENGINE-main/REGULAR_ENGINE/ModuleLoadFBX.cpp
@@ -2,7 +2,7 @@
 #include "Application.h"
 #include "ModuleLoadFBX.h"
 #include <gl/GL.h>
-
+#include "scene.h"
 
 #include "Assimp/include/cimport.h"
 #include "Assimp/include/scene.h"
@@ -21,18 +21,36 @@ ModuleLoadFBX::~ModuleLoadFBX()
 bool ModuleLoadFBX::Start()
 {
     LOG("Loading Intro assets");
-    bool ret = true;
 
+    const char* file_path;
+    const aiScene* scene;
+    bool ret = true;
+    struct aiLogStream stream;
+    stream = aiGetPredefinedLogStream(aiDefaultLogStream_DEBUGGER, nullptr);
+    aiAttachLogStream(&stream);
+    file_path = ("Assets/Ganivet.fbx");
+    scene = aiImportFile(file_path, aiProcessPreset_TargetRealtime_MaxQuality);
+    M_mesh* ourMesh = new M_mesh;
+    if (scene != nullptr && scene->HasMeshes())
+    {
+        aiReleaseImport(scene);
+    }
+    ourMesh.num_vertices = aiMesh->mNumVertices;
+    ourMesh.vertices = new float[ourMesh.num_vertices * 3];
+    memcpy(ourMesh.vertices, aiMesh->mVertices, sizeof(float) * ourMesh.num_vertices * 3);
     return ret;
 }
 
 bool ModuleLoadFBX::PreUpdate() {
-
+    // Stream log messages to Debug window
+    
     return true;
 }
 
 update_status ModuleLoadFBX::Update(float dt)
 {
+    
+
     return UPDATE_CONTINUE;
 }
 
@@ -44,5 +62,7 @@ bool ModuleLoadFBX::PostUpdate() {
 
 bool ModuleLoadFBX::CleanUp()
 {
+    aiDetachAllLogStreams();
     return true;
 }
+
--- REGULAR_ENGINE-main/REGULAR_ENGINE/ModuleLoadFBX.h
+++ REGULAR_ENGINE-main/REGULAR_ENGINE/ModuleLoadFBX.h
@@ -13,4 +13,20 @@ public:
 	bool PreUpdate();
 	bool CleanUp();
 
+	uint id_index = 0; // index in VRAM
+	uint num_index = 0;
+	uint* index = nullptr;
+	uint id_vertex = 0; // unique vertex in VRAM
+	uint num_vertex = 0;
+	float* vertex = nullptr;
+
+};
+
+class M_mesh : public Module {
+public:
+	M_mesh(Application* app, bool start_enabled = true);
+	~M_mesh();
+
+	int num_vertices;
+
 };
