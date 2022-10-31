ded43ad4c47588dbbda17866fb9269e9ad231cfe
Pau <pau.olmos2002@gmail.com>
2022-10-31 13:45:04 +0100

    A

--- REGULAR_ENGINE-main/REGULAR_ENGINE/ModuleGameObjects.cpp
+++ REGULAR_ENGINE-main/REGULAR_ENGINE/ModuleGameObjects.cpp
@@ -1,5 +1,4 @@
 #include "Application.h"
-
 #include "ModuleGameObjects.h"
 
 ModuleGameObjects::ModuleGameObjects(Application* app, bool start_enabled):Module(app, start_enabled)
@@ -12,15 +11,20 @@ bool ModuleGameObjects::Start()
 	return true;
 }
 
+update_status ModuleGameObjects::Update(float dt)
+{
+
+
+
+	return UPDATE_CONTINUE;
+}
+
 update_status ModuleGameObjects::PostUpdate()
 {
 	return UPDATE_CONTINUE;
 }
 
-update_status ModuleGameObjects::Update(float dt)
-{
-	return UPDATE_CONTINUE;
-}
+
 
 bool ModuleGameObjects::CleanUp()
 {
